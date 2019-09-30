6f35d31569d10eb673339d80435ac4ffc10994ec
gemac2 <gregorymachacon@hotmail.com>
2019-09-30 16:30:19 -0500

    exercicesfixed

---
+++ form.js
@@ -0,0 +1,31 @@
+
+function getDate (){
+    
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var phone = document.getElementById("phone").value;
+    var address = document.getElementById("address").value;
+   
+    var element = document.createElement("p");
+    var content= document.createTextNode("First Name:"+ " "+firstName)
+    element.appendChild(content)
+    document.getElementById("dates").appendChild(element);
+
+    var element = document.createElement("p");
+    var content= document.createTextNode("Last Name:"+ " "+lastName)
+    element.appendChild(content)
+    document.getElementById("dates").appendChild(element);
+
+    var element = document.createElement("p");
+    var content= document.createTextNode("Phone Number:"+ " "+phone)
+    element.appendChild(content)
+    document.getElementById("dates").appendChild(element);
+
+    var element = document.createElement("p");
+    var content= document.createTextNode("Address:"+ " "+address)
+    element.appendChild(content)
+    document.getElementById("dates").appendChild(element);
+   
+}
+
+
