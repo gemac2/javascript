33e7176b3a18802deda74677006412209377fab1
gemac2 <gregorymachacon@hotmail.com>
2019-10-01 15:40:06 -0500

    upload exercises3

---
+++ exercises3.js
@@ -0,0 +1,14 @@
+let number1 = 5;
+let number2 = 8;
+if (number1<number2){
+    alert("number1 is not older than number2")
+}
+if (number2>0){
+    alert("number2 is positive")
+}
+if(number1<0 || number1 !=0){
+    alert("number1 is negative or diferent of 0")
+}
+if (number1+1<=number2){
+    alert("Increasing the value of number1 by 1 unit does not make it greater than or equal to number2")
+}
