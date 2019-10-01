cd40c1d8d035c846b8d5fe2beab54467a9aa7b0f
gemac2 <gregorymachacon@hotmail.com>
2019-10-01 15:16:00 -0500

    exercises2

---
+++ exercises2.js
@@ -0,0 +1,25 @@
+let values = [true, 5, false, "hello", "goodbye", 2];
+
+//determine which text is greater and use booleans
+let reply1=values[0]
+let reply2=values[2]
+let text1 = values[3];
+let text2 = values[4];
+  result = text1>text2;
+   alert(reply1);
+  result = text1<text2;
+  alert(reply2);
+  
+ 
+//mathematical operations with two numbers of array
+
+let number1 = values[1];
+let number2 = values[5];
+let sum = number1 + number2;
+ alert(sum);
+let subtraction = number1 - number2; 
+alert(subtraction);
+let multiplication = number1 * number2; 
+alert(multiplication);
+let division = number1 / number2; 
+alert(division);
